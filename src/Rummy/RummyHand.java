d46032cd755bf45fc41986cb1fa88862eb6fed6c
unknown <meanil@L-MAA-25005052.paypalcorp.com>
2016-08-10 18:53:15 +0530

    added structure for assessment

--- src/Rummy/RummyHand.java
+++ src/Rummy/RummyHand.java
@@ -3,6 +3,8 @@ package Rummy;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import com.example.cardGame.Hand;
+
 import GameComponents.*;
 
 public class RummyHand extends Hand{
@@ -13,6 +15,42 @@ public class RummyHand extends Hand{
 	
 	public void sortCards(){
 		Collections.sort(cardsInHand);
+	}
+	
+	public int evaluate(Hand playerCardsInHand)
+	{
+		int numMethod1=method1(playerCardsInHand);
+		int numMethod2=method2(playerCardsInHand);
+		int numMethod3=method3(playerCardsInHand);
+		int min=numMethod2;
+		if(numMethod1<numMethod2){
+			min=numMethod1;	
+		}
+		if(min>numMethod3)
+		{
+			min=numMethod3;
+		}
+		return min;
+		
+	}
+	
+	//Check for sequence 553
+	public int method1(Hand h){
+		
+		
+		return 0;
+	}
+	
+	//Check for sequence 544
+	public int method2(Hand h){
+		
+		return 0;
+	}
+	
+	//Check for sequence4333
+	public int method3(Hand h){
+		
+		return 0;
 	}
 	
 	

cda050b4cf7e4569073a6793f98dff9431222280
A K <abiak@paypalcorp.com>
2016-08-11 09:14:40 +0530

    Solving Merge Conflicts

--- src/Driver.java
+++ src/Driver.java
@@ -9,7 +9,7 @@ public class Driver {
 			Deck deck = new Deck(2, true);
 			Card joker;
 			deck.shuffle();
-<<<<<<< HEAD
+
 //			for(int i=0;i<13;i++){
 //				hand.addCard(deck.removeCardOnTop());
 //			}	
@@ -26,24 +26,7 @@ public class Driver {
 			hand.addCard(new Card("7","SPADE"));
 			hand.addCard(new Card("7","DIAMOND"));
 			hand.addCard(new Card("Q","SPADE"));
-=======
-			for(int i=0;i<13;i++){
-				hand.addCard(deck.removeCardOnTop());
-			}	
-//			hand.addCard(new Card("2","SPADE"));
-//			hand.addCard(new Card("3","SPADE"));
-//			hand.addCard(new Card("4","SPADE"));
-//			hand.addCard(new Card("T","SPADE"));
-//			hand.addCard(new Card("T","HEART"));
-//			hand.addCard(new Card("T","DIAMOND"));
-//			hand.addCard(new Card("5","SPADE"));
-//			hand.addCard(new Card("5","CLUB"));
-//			hand.addCard(new Card("5","HEART"));
-//			hand.addCard(new Card("5","DIAMONS"));
-//			hand.addCard(new Card("7","SPADE"));
-//			hand.addCard(new Card("7","DIAMOND"));
-//			hand.addCard(new Card("Q","SPADE"));
->>>>>>> 74543080ed81ce375d4fa803cc6e714404af2e7a
+
 			
 			joker=deck.removeCardOnTop();
 			//joker=new Card("7","hearts");
